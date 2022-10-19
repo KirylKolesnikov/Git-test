cb032b2b420574f3375efa1b2f609c6f761a72e5
Kiryl Kolesnikov <ziga6552@gmail.com>
2022-10-19 22:51:30 +0300

    js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,34 @@ $(function(){
     $('.rev-slide').slick({
         infinite: true,
         slidesToShow: 2,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
         dots: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // Адаптив
+          
+        ]
       });
 })
