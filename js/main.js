5ecb00c112a42dcafdf4553842a22bac3c7cb554
Kiryl Kolesnikov <ziga6552@gmail.com>
2022-10-20 15:32:08 +0300

    end

--- js/main.js
+++ js/main.js
@@ -25,6 +25,7 @@ anchors.forEach(anchor => {
     function closeMenu (){
       nav.classList.remove('open');
       navBtnImg.src = "./img/icons/nav-open.svg";
+      body.classList.toggle('.noscroll');
       
 
     }
